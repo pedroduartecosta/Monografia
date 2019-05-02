6ad4b6a7d9608f7830194af31e8390629495b34d
Pedro Costa <pedrodc23@gmail.com>
2019-05-02 15:42:58 +0100

    working on eth oracle

--- eos-oracle/taikai-contracts/docker/deploy_contract.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-WALLET_URL=http://35.195.1.81:8899
-NODE_URL=http://35.195.1.81:8888
-CLEOS_OPTIONS="--wallet-url=$WALLET_URL --url=$NODE_URL"
-
-cleos $CLEOS_OPTIONS set contract hello ./hello -p hello@active
