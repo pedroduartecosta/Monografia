fd4a79b76b934f2abbaf2ce85c5b7556ab9bd121
Pedro Costa <pedrodc23@gmail.com>
2019-05-15 14:16:52 +0100

    working oracle :rocket:

--- eth-oracle/oracle-server/src/ethereum.js
+++ eth-oracle/oracle-server/src/ethereum.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 import HDWalletProvider from "truffle-hdwallet-provider";
 import Web3 from "web3";
 
-const web3 = new Web3(new HDWalletProvider(process.env.MNEMONIC, process.env.WEB3_PROVIDER_ADDRESS));
+//const web3 = new Web3(new HDWalletProvider(process.env.MNEMONIC, process.env.WEB3_PROVIDER_ADDRESS));
+const web3 = new Web3(new Web3.providers.HttpProvider(process.env.WEB3_PROVIDER_ADDRESS));
 const abi = JSON.parse(process.env.ABI);
 const address = process.env.CONTRACT_ADDRESS;
 const contract = web3.eth.contract(abi).at(address);
@@ -20,18 +21,26 @@ const account = () => {
   });
 };
 
-export const updateWeather = ({ weatherDescription, temperature, humidity, visibility, windSpeed, windDirection, windGust }) => {
+export const updateWeather = ({
+  weatherDescription,
+  temperature,
+  humidity,
+  visibility,
+  windSpeed,
+  windDirection,
+  windGust
+}) => {
   return new Promise((resolve, reject) => {
     account().then(account => {
-      contract.updateWeather(weatherDescription, temperature, humidity, visibility, windSpeed, windDirection, windGust,
-        { from: account }, (err, res) => {
-          if (err === null) {
-            resolve(res);
-          } else {
-            reject(err);
-          }
+      contract.updateWeather(weatherDescription, temperature, humidity, visibility, windSpeed, windDirection, windGust, {
+        from: account
+      }, (err, res) => {
+        if (err === null) {
+          resolve(res);
+        } else {
+          reject(err);
         }
-      );
+      });
     }).catch(error => reject(error));
   });
 };
